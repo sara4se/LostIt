51ed3ab105555c1cab89bb9e5407cb5b9cecb020
sara <sara.ahumidi4@gmail.com>
2023-02-01 19:47:35 +0300

    Initial Commit

---
+++ Dalti/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dalti
+//
+//  Created by Sara Alhumidi on 10/07/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dalti/DaltiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaltiApp.swift
+//  Dalti
+//
+//  Created by Sara Alhumidi on 10/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DaltiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
